cf2d5c6f81365a655373df06cea3c85a78636834
mirrormove <dsilva1295@gmail.com>
2017-12-06 01:51:27 +0000

    Camera centered

--- Chrono Squad/Assets/Scripts/CamaraFollow.cs
+++ Chrono Squad/Assets/Scripts/CamaraFollow.cs
@@ -6,22 +6,19 @@ public class CamaraFollow : MonoBehaviour {
 
     private Vector2 velocity;
 
-    public float smoothTimeY;
     public float smoothTimeX;
-    public Vector2 offset = new Vector2();
 
     public GameObject player;
 
 	// Use this for initialization
 	void Start () {
-        player = GameObject.FindGameObjectWithTag("Player");
+
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
-        //float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
     
-        transform.position = new Vector3(posX+offset.x,0, transform.position.z);
+        transform.position = new Vector3(posX,0, transform.position.z);
     }   
 }
