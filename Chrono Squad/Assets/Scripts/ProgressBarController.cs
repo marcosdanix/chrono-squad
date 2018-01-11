d8840930ceb217d7d18f591fcd2c354a33e4ff47
DMTunes <xdiogo6@hotmail.com>
2018-01-11 16:10:27 +0000

    New canvas and time progress bar
    Helps the player knowing his "place in time"
    Adding actual time numbers on the side might be helpfull

---
+++ Chrono Squad/Assets/Scripts/ProgressBarController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarController : MonoBehaviour
+{
+    public Image ProgressMarker;
+    public RectTransform rectTransform;
+    int counter = 0;
+    bool rewindCheck = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            rewindCheck = true;
+            Rewind();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            rewindCheck = false;
+        }
+
+
+        if (!rewindCheck)
+            {
+                Move();
+            }
+        
+    }
+
+    public void Move()
+    {
+        if(counter < 600)
+        {
+            rectTransform.localPosition += Vector3.right * 950 / 600;
+            counter++;
+        }
+    }
+
+    public void Rewind()
+    {
+        if (counter > 0)
+        {
+            rectTransform.localPosition += Vector3.left * 950 / 600;
+            counter--;
+        }
+    }
+}
