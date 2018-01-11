60afa22b072f2543a10b20bf2d1c73372a2f4a1e
DMTunes <xdiogo6@hotmail.com>
2018-01-11 20:14:25 +0000

    Music
    Added background music and boss fight music
    Music resets everytime player hits the checkpoint
    Winning sound not implemented

---
+++ Chrono Squad/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    AudioSource source;
+    Collider2D checkpoint;
+    Collider2D playerCollider;
+    public AudioClip[] clips;
+    public GameObject player;
+    
+
+	// Use this for initialization
+	void Start () {
+        source = GetComponent<AudioSource>();
+        checkpoint = GetComponentInChildren<Collider2D>();
+        source.clip = clips[0];
+        source.Play();
+        playerCollider = player.GetComponent<Collider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //OnTriggerEnter2D(checkpoint);
+        if (checkpoint.IsTouching(playerCollider))
+        {
+            source.clip = clips[1];
+            source.Play();
+        }
+	}
+
+}
